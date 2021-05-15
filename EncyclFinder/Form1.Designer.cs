ef6c0311f4be0c6ff1d34fe30be77cbf803bf46e
msdos41 <tjm.mosquito@gmail.com>
2021-05-15 19:32:46 +0800

    Upload first version.

---
+++ EncyclFinder/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EncyclFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowserContent = new System.Windows.Forms.WebBrowser();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // webBrowserContent
+            // 
+            this.webBrowserContent.Location = new System.Drawing.Point(279, 12);
+            this.webBrowserContent.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserContent.Name = "webBrowserContent";
+            this.webBrowserContent.Size = new System.Drawing.Size(1131, 772);
+            this.webBrowserContent.TabIndex = 2;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(198, 12);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.HorizontalExtent = 100;
+            this.listBoxResult.HorizontalScrollbar = true;
+            this.listBoxResult.ItemHeight = 12;
+            this.listBoxResult.Location = new System.Drawing.Point(12, 49);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(261, 736);
+            this.listBoxResult.TabIndex = 4;
+            this.listBoxResult.DoubleClick += new System.EventHandler(this.listBoxResult_MouseDoubleClick);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(13, 13);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(179, 21);
+            this.textBoxInput.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1422, 796);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.webBrowserContent);
+            this.Name = "Form1";
+            this.Text = "Encyclopedia Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowserContent;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.TextBox textBoxInput;
+
+    }
+}
+
---
+++ EncyclFinder/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+using System.Net;
+using Microsoft.Win32;
+
+namespace EncyclFinder
+{
+    public partial class Form1 : Form
+    {
+        public static List<FileInfo> _lstFileAll = new List<FileInfo>();
+        public static List<string> _lstFilePathAll = new List<string>();
+        public static List<string> _lstSearchResultPath = new List<string>();
+        public static List<string> _lstSearchResultTitle = new List<string>();
+        public static int _iCountSearch = 0;
+        public static string _sRootPath = "";
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            _sRootPath = GetCATIAPath();
+            if (_sRootPath=="")
+            {
+                MessageBox.Show("Cannot find CATIA program in computer!");
+                Application.Exit();
+            }
+        }
+
+        public string GetCATIAPath()
+        {
+            try
+            {
+                string softName = "CNEXT";
+                string strKeyName = string.Empty;
+                string softPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\";
+                RegistryKey regKey = Registry.LocalMachine;
+                RegistryKey regSubKey = regKey.OpenSubKey(softPath + softName + ".exe", false);
+
+                object objResult = regSubKey.GetValue(strKeyName);
+                RegistryValueKind regValueKind = regSubKey.GetValueKind(strKeyName);
+                if (regValueKind == Microsoft.Win32.RegistryValueKind.String)
+                {
+                    return objResult.ToString();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return "";
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            _iCountSearch++;
+            this.listBoxResult.Items.Clear();
+            this.webBrowserContent.Navigate("about:blank");
+            _lstSearchResultPath.Clear();
+            _lstSearchResultTitle.Clear();
+
+            if (_iCountSearch==1)
+            {
+                int iIndex = _sRootPath.IndexOf("win_b64");
+                if (iIndex<0)
+                {
+                    MessageBox.Show("CATIA Program path NOT include win_b64 folder!");
+                    Application.Exit();
+                }
+                string sPath = _sRootPath.Substring(0, iIndex) + "CAADoc\\Doc\\online\\";
+                FindFile(sPath, ref _lstFilePathAll);
+                //FindFile("C:\\CATIA\\B26\\CAADoc\\Doc\\online\\",ref _lstFilePathAll);
+            }
+
+            string sInput = textBoxInput.Text;
+            
+            SearchFilesFromKeyWord(sInput, _lstFilePathAll, ref _lstSearchResultPath,ref _lstSearchResultTitle);
+            //foreach (string s in _lstSearchResultPath)
+            //{
+            //    int iIndex = s.LastIndexOf("\\");
+            //    string sFileName = s.Substring(iIndex + 1, s.Length - iIndex-1);
+            //    listBoxResult.Items.Add(sFileName);
+            //}
+            //
+            int iSize = _lstSearchResultPath.Count;
+            for (int i = 0; i < iSize;i++ )
+            {
+                string sTitle = _lstSearchResultTitle[i];
+                if (sTitle=="")
+                {
+                    string sPath = _lstSearchResultPath[i];
+                    int iIndex = sPath.LastIndexOf("\\");
+                    sTitle = sPath.Substring(iIndex + 1, sPath.Length - iIndex - 1);
+                }
+                listBoxResult.Items.Add(sTitle);
+            }
+        }
+
+        private void listBoxResult_MouseDoubleClick(object sender, EventArgs e)
+        {
+            int iSelectIndex = listBoxResult.SelectedIndex;
+            if (iSelectIndex>=0)
+            {
+                string sPath = _lstSearchResultPath[iSelectIndex];
+                this.webBrowserContent.Navigate(sPath);
+            }
+        }
+
+        public void FindFile(string dirPath,ref List<string> oLstPath)
+        {
+            DirectoryInfo Dir=new DirectoryInfo(dirPath);
+            try
+            {
+                foreach (DirectoryInfo d in Dir.GetDirectories())    //查找子目录
+                {
+
+                    //listBoxResult.Items.Add(d.ToString() + "\\"); //listBox1中填加目录名
+                    FindFile(Dir + d.ToString() + "\\",ref oLstPath);
+
+                }
+
+                foreach (FileInfo f in Dir.GetFiles("*.*"))    //查找文件
+                {
+                    if (f.Extension.Equals(".htm") || f.Extension.Equals(".html"))
+                    {
+                        //listBoxResult.Items.Add(f.ToString()); //listBox1中填加文件名
+                        oLstPath.Add(Dir.ToString()+f.ToString());
+                        
+                    }
+                }
+            }
+
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public void GetContentFromHTML(string sPath, ref string oContent)
+        {
+            try
+            {
+                //指定请求
+                WebRequest request = WebRequest.Create(sPath);
+
+                //得到返回
+                WebResponse response = request.GetResponse();
+
+                //得到流
+                Stream recStream = response.GetResponseStream();
+
+                //编码方式
+                Encoding ec = Encoding.UTF8;
+
+                //指定转换为gb2312编码
+                StreamReader sr = new StreamReader(recStream, ec);
+
+                //以字符串方式得到网页内容
+                oContent = sr.ReadToEnd();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void SearchFilesFromKeyWord(string isKeyWord, List<string> ilstFileAll, ref List<string> olstFind,ref List<string> olstFindTitle)
+        {
+            foreach (string sPath in ilstFileAll)
+            {
+                string sContent="";
+                GetContentFromHTML(sPath, ref sContent);
+                int iIndex = sContent.IndexOf(isKeyWord);
+                if (iIndex>-1)
+                {
+                    olstFind.Add(sPath);
+
+                    //获取文档中的title
+                    string sTitle = "";
+                    int iIndexTitleStart = sContent.IndexOf("<title>");
+                    int iIndexTitleEnd = sContent.IndexOf("</title>");
+                    if (iIndexTitleStart<iIndexTitleEnd && iIndexTitleStart>-1)
+                    {
+                        sTitle = sContent.Substring(0, iIndexTitleEnd);
+                        iIndexTitleStart = iIndexTitleStart+7;
+                        sTitle = sTitle.Substring(iIndexTitleStart, sTitle.Length - iIndexTitleStart);
+                    }
+
+                    olstFindTitle.Add(sTitle);
+                }
+            }
+        }
+    }
+
+    class GeneralCls
+    {
+
+    }
+}
